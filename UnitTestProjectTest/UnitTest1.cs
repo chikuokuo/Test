47997b8f0e63544b068f90e75a291089c179631b
Bell Chen Chi Kuo <chikuokuo@msn.com>
2017-07-31 14:30:57 +0800

    init

---
+++ UnitTestProjectTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestProjectTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
